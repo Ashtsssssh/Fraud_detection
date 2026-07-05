ffbf5d805c7ef144ef2550df06de0d913c9b1ce3
Ashtsssssh <ashutoshx.627@gmail.com>
2026-07-05 12:55:53 +0530

    Module 1: Project Setup and Configurations

---
+++ src/config.py
@@ -0,0 +1,8 @@
+DATA_PATH = "data/raw/SAML_D.csv"
+PROCESSED_PATH = "data/processed/features.csv"
+MODEL_PATH = "models/aml_model.pkl"
+
+TARGET = "is_suspicious"
+
+RANDOM_STATE = 42
+TEST_SIZE = 0.2
